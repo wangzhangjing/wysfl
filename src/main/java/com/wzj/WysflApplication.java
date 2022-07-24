d1c58aed4cc9d3301fc453962e9dde67a32ba7bf
王章敬 <1550730569@qq.com>
2022-07-24 21:32:15 +0800

    1修改

---
+++ src/main/java/com/wzj/WysflApplication.java
@@ -0,0 +1,13 @@
+package com.wzj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WysflApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WysflApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wzj/WysflApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WysflApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
